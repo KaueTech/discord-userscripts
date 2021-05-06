5227ee1281502622cbe2c9741d17990944d3d3d3
Kauê Dias <kauedias.dev@gmail.com>
2021-05-06 11:37:19 -0300

    First commit and add token scraper script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# discord-userscripts
-Alguns userscripts que fiz para usar no Google Meet.
+## Tampermonkey
+> :warning: **É necessário ter o Tampermonkey instalado para instalar os scripts.**
+
+Tampermonkey é uma extensão grátis para seu navegador e o gerenciador mais popular de scripts de usuário para navegadores baseados em Blink e WebKit, tais como Chrome, Microsoft Edge, Safari, Opera Next, e Firefox.
+\
+\
+<a href="https://www.tampermonkey.net/" target="_blank"><img src="https://dabuttonfactory.com/button.png?t=INSTALAR&f=Overlock-Bold&ts=28&tc=fff&hp=45&vp=20&w=150&h=50&c=11&bgt=unicolored&bgc=14c75f" alt="Instalar Tampermonkey" width="100" ></a>
+
+## Userscripts
+
+### Get Discord Token
+Um script para procurar seu token do discord apertando Ctrl + Shift + ]
+\
+<a href="https://github.com/KaueTech/discord-userscripts/raw/master/token-scraper.user.js" target="_blank"><img src="https://dabuttonfactory.com/button.png?t=INSTALAR&f=Overlock-Bold&ts=28&tc=fff&hp=45&vp=20&w=150&h=50&c=11&bgt=unicolored&bgc=14c75f" alt="Instalar script Chat Presence List" width="100" ></a>
---
+++ token-scraper.user.js
@@ -0,0 +1,129 @@
+// ==UserScript==
+// @name         Token Scraper - Ctrl + Shift + ]
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Um script para procurar seu token do discord apertando Ctrl + Shift + ].
+// @author       @KaueTech - Kauê#0220
+// @match        https://discord.com/*
+// @icon         https://www.google.com/s2/favicons?domain=discord.com
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+   // impliment localstorage behavior using cookie
+   //---------------------------------------------
+   if(!window.localStorage) {
+      Object.defineProperty(window, "localStorage", new(function () {
+         var aKeys = [],
+            oStorage = {};
+         Object.defineProperty(oStorage, "getItem", {
+            value: function (sKey) {
+               return this[sKey] ? this[sKey] : null;
+            },
+            writable: false,
+            configurable: false,
+            enumerable: false
+         });
+         Object.defineProperty(oStorage, "key", {
+            value: function (nKeyId) {
+               return aKeys[nKeyId];
+            },
+            writable: false,
+            configurable: false,
+            enumerable: false
+         });
+         Object.defineProperty(oStorage, "setItem", {
+            value: function (sKey, sValue) {
+               if(!sKey) {
+                  return;
+               }
+               document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+            },
+            writable: false,
+            configurable: false,
+            enumerable: false
+         });
+         Object.defineProperty(oStorage, "length", {
+            get: function () {
+               return aKeys.length;
+            },
+            configurable: false,
+            enumerable: false
+         });
+         Object.defineProperty(oStorage, "removeItem", {
+            value: function (sKey) {
+               if(!sKey) {
+                  return;
+               }
+               document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+            },
+            writable: false,
+            configurable: false,
+            enumerable: false
+         });
+         Object.defineProperty(oStorage, "clear", {
+            value: function () {
+               if(!aKeys.length) {
+                  return;
+               }
+               for(var sKey in aKeys) {
+                  document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+               }
+            },
+            writable: false,
+            configurable: false,
+            enumerable: false
+         });
+         this.get = function () {
+            var iThisIndx;
+            for(var sKey in oStorage) {
+               iThisIndx = aKeys.indexOf(sKey);
+               if(iThisIndx === -1) {
+                  oStorage.setItem(sKey, oStorage[sKey]);
+               } else {
+                  aKeys.splice(iThisIndx, 1);
+               }
+               delete oStorage[sKey];
+            }
+            for(aKeys; aKeys.length > 0; aKeys.splice(0, 1)) {
+               oStorage.removeItem(aKeys[0]);
+            }
+            for(var aCouple, iKey, nIdx = 0, aCouples = document.cookie.split(/\s*;\s*/); nIdx < aCouples.length; nIdx++) {
+               aCouple = aCouples[nIdx].split(/\s*=\s*/);
+               if(aCouple.length > 1) {
+                  oStorage[iKey = unescape(aCouple[0])] = unescape(aCouple[1]);
+                  aKeys.push(iKey);
+               }
+            }
+            return oStorage;
+         };
+         this.configurable = false;
+         this.enumerable = true;
+      })());
+   }
+   //---------------------------------------------------
+
+    var userToken = localStorage.getItem('token')
+
+    if (userToken)
+        userToken = userToken.slice(1, -1);
+    else
+        userToken = 'Token not found'
+
+    var isKeyPressed = {};
+
+    document.onkeydown = (event) => {
+        isKeyPressed[event.key.toLocaleLowerCase()] = true;
+
+        if (event.ctrlKey && event.shiftKey && (isKeyPressed[']'] || isKeyPressed['}'])) {
+            isKeyPressed = {}
+
+            prompt('Your user token:', userToken)
+        }
+    };
+
+    document.onkeyup = (event) => isKeyPressed = {}
+})();
